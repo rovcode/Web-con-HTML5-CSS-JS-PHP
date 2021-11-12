39c0225c1bfe90dde442210f8333ed91dae661b9
Rony Velásquez <velasquezcordovar@gmail.com>
2021-11-12 17:06:22 -0500

    Avanzando front

--- View/snippets/head.php
+++ View/snippets/head.php
@@ -9,6 +9,7 @@ $Config = new Config();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#ddb293">
     <?php include "css.php"?>
     <link rel="shortcut icon" href="Public/asset/img/ico.ico">
     <title><?php $Config->setName()?></title>
