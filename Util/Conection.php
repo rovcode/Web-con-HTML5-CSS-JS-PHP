9a53221d3095918ffdd284f14824953478cbe61b
Rony Velásquez <velasquezcordovar@gmail.com>
2021-11-26 16:03:06 -0500

    Conexión cambio

--- Util/Conection.php
+++ Util/Conection.php
@@ -3,15 +3,15 @@
 class Conexion
 {
     protected $VAR_PDO = null;
-    protected $VAR_DB = "dbstore";
-    protected $VAR_SERVER = "localhost:3306";
+    protected $VAR_DB = "c3u2yq869upbm7xf";
+    protected $VAR_SERVER = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
     public function getConexion()
     {
         try
         {
-            $this->VAR_PDO = new PDO("mysql:host=" . $this->VAR_SERVER . ";dbname=" . $this->VAR_DB . "", "root", "");
+            $this->VAR_PDO = new PDO("mysql:host=" . $this->VAR_SERVER . ";dbname=" . $this->VAR_DB . "", "f9accbyl9g2ctzzp", "nnkardo1ar0mazk6");
             if ($this->VAR_PDO) {
-               echo "Conectado a <b>" . $this->VAR_DB . "</b>";
+                // echo "Conectado a <b>" . $this->VAR_DB . "</b>";
             }
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage . "<br>";
@@ -19,3 +19,4 @@ class Conexion
         return $this->VAR_PDO;
     }
 }
+
