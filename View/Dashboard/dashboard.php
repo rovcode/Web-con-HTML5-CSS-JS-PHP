3b91523b884fdbc9ee8c07a98a932839fa4f311f
Rony Velásquez <velasquezcordovar@gmail.com>
2021-11-26 15:55:57 -0500

    Cambios#

--- View/Dashboard/dashboard.php
+++ View/Dashboard/dashboard.php
@@ -6,7 +6,13 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard </h1>
+                            <h1 class="m-0">Dashboard 
+                            <?php require_once '../../Util/Conection.php';
+                             $cn1=new Conexion();
+                             $cn1-> getConexion();
+                            ?>
+
+                            </h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
