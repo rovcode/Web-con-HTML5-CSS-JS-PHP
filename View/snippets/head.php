ea11bb25fc37ec32b3cc0bf26fb430695c79e06b
Rony Velásquez <velasquezcordovar@gmail.com>
2021-12-10 14:06:00 -0500

    PWA

--- View/snippets/head.php
+++ View/snippets/head.php
@@ -11,6 +11,8 @@ $Config = new Config();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#ddb293">
+    <link rel="manifest" href="manifest.json">
+    <link rel="apple-touch-icon" href="Public/asset/img/ico.jpg">
     <?php include "css.php"?>
     <link rel="shortcut icon" href="Public/asset/img/ico.ico">
     <title><?php $Config->setName()?></title>
